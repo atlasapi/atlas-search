78baf18befdd3c2e67017b67d13b241f6acec34a
Tom McAdam <tom@metabroadcast.com>
2013-01-21 15:38:46 +0000

    Change way Cassandra client is instantiated

--- src/main/java/org/atlasapi/search/AtlasSearchModule.java
+++ src/main/java/org/atlasapi/search/AtlasSearchModule.java
@@ -1,33 +1,8 @@
 package org.atlasapi.search;
 
-
-import com.google.common.base.Function;
-import com.google.common.base.Predicates;
-import com.google.common.base.Splitter;
-
-import org.atlasapi.media.entity.Publisher;
-import org.atlasapi.persistence.content.ContentCategory;
-import org.atlasapi.persistence.content.listing.ContentListingCriteria;
-import org.atlasapi.persistence.content.mongo.MongoContentLister;
-import org.atlasapi.persistence.content.mongo.MongoContentResolver;
-import org.atlasapi.persistence.content.mongo.MongoPersonStore;
-import org.atlasapi.search.searcher.LuceneSearcherProbe;
-import org.atlasapi.search.searcher.ReloadingContentBootstrapper;
-import org.atlasapi.search.view.JsonSearchResultsView;
-import org.atlasapi.search.www.WebAwareModule;
-import org.springframework.context.annotation.Bean;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.ImmutableList.Builder;
-import com.google.common.collect.Lists;
-import com.metabroadcast.common.health.HealthProbe;
-import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
-import com.metabroadcast.common.properties.Configurer;
-import com.metabroadcast.common.webapp.health.HealthController;
-import com.mongodb.Mongo;
-import com.mongodb.ReadPreference;
-import com.mongodb.ServerAddress;
+import static org.atlasapi.persistence.cassandra.CassandraSchema.CLUSTER;
+import static org.atlasapi.persistence.cassandra.CassandraSchema.getKeyspace;
+import static org.atlasapi.persistence.content.listing.ContentListingCriteria.defaultCriteria;
 
 import java.io.File;
 import java.net.UnknownHostException;
@@ -35,11 +10,43 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.persistence.content.ContentCategory;
 import org.atlasapi.persistence.content.cassandra.CassandraContentStore;
+import org.atlasapi.persistence.content.listing.ContentListingCriteria;
+import org.atlasapi.persistence.content.mongo.MongoContentLister;
+import org.atlasapi.persistence.content.mongo.MongoContentResolver;
+import org.atlasapi.persistence.content.mongo.MongoPersonStore;
 import org.atlasapi.search.loader.ContentBootstrapper;
 import org.atlasapi.search.searcher.LuceneContentIndex;
+import org.atlasapi.search.searcher.LuceneSearcherProbe;
+import org.atlasapi.search.searcher.ReloadingContentBootstrapper;
+import org.atlasapi.search.view.JsonSearchResultsView;
+import org.atlasapi.search.www.WebAwareModule;
 import org.joda.time.Duration;
-import static org.atlasapi.persistence.content.listing.ContentListingCriteria.defaultCriteria;
+import org.springframework.context.annotation.Bean;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicates;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+import com.google.common.collect.Iterables;
+import com.metabroadcast.common.health.HealthProbe;
+import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
+import com.metabroadcast.common.properties.Configurer;
+import com.metabroadcast.common.webapp.health.HealthController;
+import com.mongodb.Mongo;
+import com.mongodb.ReadPreference;
+import com.mongodb.ServerAddress;
+import com.netflix.astyanax.AstyanaxContext;
+import com.netflix.astyanax.Keyspace;
+import com.netflix.astyanax.connectionpool.NodeDiscoveryType;
+import com.netflix.astyanax.connectionpool.impl.ConnectionPoolConfigurationImpl;
+import com.netflix.astyanax.connectionpool.impl.CountingConnectionPoolMonitor;
+import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
+import com.netflix.astyanax.thrift.ThriftFamilyFactory;
 
 public class AtlasSearchModule extends WebAwareModule {
 
@@ -128,20 +135,23 @@ public class AtlasSearchModule extends WebAwareModule {
 	}
     
     public @Bean CassandraContentStore cassandra() {
-		try {
-		    System.out.println(cassandraEnv);
-			CassandraContentStore cassandraContentStore = new CassandraContentStore(
-			    cassandraEnv,
-			    Lists.newArrayList(Splitter.on(',').split(cassandraSeeds)), 
-                Integer.parseInt(cassandraPort), 
-                Runtime.getRuntime().availableProcessors() * 10, 
-                Integer.parseInt(cassandraConnectionTimeout), 
-                Integer.parseInt(cassandraRequestTimeout));
+        try {
+            AstyanaxContext<Keyspace> cassandraContext = new AstyanaxContext.Builder().forCluster(CLUSTER).forKeyspace(getKeyspace(cassandraEnv)).
+                withAstyanaxConfiguration(new AstyanaxConfigurationImpl().setDiscoveryType(NodeDiscoveryType.NONE)).
+                withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl(CLUSTER).setPort(Integer.parseInt(cassandraPort)).
+                setMaxBlockedThreadsPerHost(Runtime.getRuntime().availableProcessors() * 10).
+                setMaxConnsPerHost(Runtime.getRuntime().availableProcessors() * 10).
+                setConnectTimeout(Integer.parseInt(cassandraConnectionTimeout)).
+                setSeeds(cassandraSeeds)).
+                withConnectionPoolMonitor(new CountingConnectionPoolMonitor()).
+                buildKeyspace(ThriftFamilyFactory.getInstance());
+            CassandraContentStore cassandraContentStore = new CassandraContentStore(cassandraContext, Integer.parseInt(cassandraRequestTimeout));
+            cassandraContext.start();
             cassandraContentStore.init();
             return cassandraContentStore;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 	}
     
     private List<ServerAddress> mongoHosts() {
