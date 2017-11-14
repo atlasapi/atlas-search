c7d0c161d7888021da1866d2854ab6799bc12ad8
@alianos- <alianos.home@gmail.com>
2017-11-14 16:22:08 +0000

    added support for publisher reindexin to the api

--- src/main/java/org/atlasapi/search/www/ContentIndexController.java
+++ src/main/java/org/atlasapi/search/www/ContentIndexController.java
@@ -1,6 +1,7 @@
 package org.atlasapi.search.www;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static org.atlasapi.persistence.content.listing.ContentListingCriteria.defaultCriteria;
 
 import java.io.IOException;
 
@@ -10,8 +11,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.atlasapi.media.entity.Described;
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.persistence.content.ContentCategory;
 import org.atlasapi.persistence.content.ContentResolver;
+import org.atlasapi.persistence.content.listing.ContentListingCriteria;
+import org.atlasapi.persistence.content.listing.MongoProgressStore;
+import org.atlasapi.persistence.content.mongo.MongoContentLister;
+import org.atlasapi.persistence.content.mongo.MongoContentResolver;
+import org.atlasapi.persistence.content.mongo.MongoPersonStore;
+import org.atlasapi.persistence.lookup.TransitiveLookupWriter;
+import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
+import org.atlasapi.persistence.lookup.mongo.MongoLookupEntryStore;
+import org.atlasapi.search.AtlasSearchModule;
+import org.atlasapi.search.loader.ContentBootstrapper;
 import org.atlasapi.search.searcher.LuceneContentIndex;
+
+import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.google.common.collect.ImmutableSet;
@@ -22,21 +39,52 @@ import com.google.common.collect.Iterables;
 public class ContentIndexController extends HttpServlet {
 
     private static final String URI_PARAMETER = "uri";
-    
+    private static final String PUBLISHER_PARAMETER = "publisher";
+
+    @Autowired
+    private MongoProgressStore progressStore;
+    @Autowired
+    private DatabasedMongo mongo;
+    @Autowired
+    private MongoContentResolver mongoContentResolver;
+
     private final ContentResolver contentResolver;
     private final LuceneContentIndex index;
-    
+
     public ContentIndexController(ContentResolver contentResolver, LuceneContentIndex index) {
         this.contentResolver = checkNotNull(contentResolver);
         this.index = checkNotNull(index);
     }
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        index.beforeContentChange();
-        index.contentChange(Iterables.filter(
-                contentResolver.findByCanonicalUris(ImmutableSet.of(request.getParameter(URI_PARAMETER))).getAllResolvedResults(), Described.class));
-        index.afterContentChange();
+        String uri = request.getParameter(URI_PARAMETER);
+        String publisher = request.getParameter(PUBLISHER_PARAMETER);
+
+        if(uri!=null) {
+            index.beforeContentChange();
+            index.contentChange(Iterables.filter(
+                    contentResolver.findByCanonicalUris(ImmutableSet.of(uri))
+                            .getAllResolvedResults(), Described.class));
+            index.afterContentChange();
+        }
+        if(publisher!=null){
+            ContentListingCriteria.Builder criteriaBuilder = defaultCriteria()
+                    .forPublishers(ImmutableSet.<Publisher>builder()
+                            .add(Publisher.valueOf(publisher))
+                            .build()
+                            .asList()
+                    );
+
+            ContentBootstrapper.BuildStep bootstrapperBuilder = ContentBootstrapper.builder()
+                    .withTaskName("owl-search-bootstrap-mongo-api-request")
+                    .withProgressStore(progressStore)
+                    .withContentLister(new MongoContentLister(mongo, mongoContentResolver))
+                    .withCriteriaBuilder(criteriaBuilder);
+
+            ContentBootstrapper build = bootstrapperBuilder.build();
+            build.loadAllIntoListener(index);
+        }
         response.getWriter().write("DONE");
     }
 }
